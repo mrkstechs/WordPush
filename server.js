913f7f064c41b29297de5382a143b981fc02b2ee
dykim <dyounggkim@gmail.com>
2022-10-24 13:32:57 +0100

    added data property to comments/posts

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const postsData = require('./db/posts.json');
 const commentsData = require('./db/comments.json');
 const userData = require('./db/users.json');
 
-const { uniqueId } = require('./functions')
+const { uniqueId, getDate } = require('./functions')
 
 //Users should be able to view other peoples' entries. (2)
 //working so far
@@ -25,10 +25,11 @@ app.get('/', (req, res)=> {
 //Working so far with req.body
 app.post('/', (req, res)=> {
     
-    let commentToAdd = req.body;
-    commentToAdd.postId = uniqueId();
+    let postToAdd = req.body;
+    postToAdd.postId = uniqueId();
+    postToAdd.date = getDate();
 
-    postsData.push(commentToAdd);
+    postsData.push(postToAdd);
 
     res.send(postsData);
 });
@@ -39,11 +40,7 @@ app.post('/comments', (req, res)=> {
     let postId = req.body.postId;
     let comment = req.body;
     comment.commentId = uniqueId();
-
-
-    // const findPost = postsData.find((post)=> {
-    //     return post.postId === postId;
-    // })
+    comment.date = getDate();
 
     const findPostIndex = postsData.findIndex((post)=> {
         return post.postId === postId;
@@ -57,14 +54,14 @@ app.post('/comments', (req, res)=> {
 //Users should be able to react to other 
 //peoples’ entries with a choice of 3 emojis. (7)
 app.post('/emoji', (req, res)=> {
-    //user clicks on post, choses emoji
-    //post=> postsData[0] 
-    //emoji=> req.body.emoji
 
-    console.log(req.body.sendEmoji);
-    console.log(postsData[0])
-    console.log(postsData[0].reactionEmoji.push(req.body.sendEmoji))
+    let postId = req.body.postId;
+    let emojiToAdd = req.body.emojiToAdd;
+    const findPostIndex = postsData.findIndex((post)=> {
+        return post.postId === postId;
+    })
 
+    postsData[findPostIndex].reactionEmoji.push(emojiToAdd)
     res.send(postsData);
 });
 
@@ -72,3 +69,4 @@ app.post('/emoji', (req, res)=> {
 app.listen(port, ()=> {
     console.log(`listening on port ${port}`)
 })
+
