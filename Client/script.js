f704af8de647afabb63036f81b3dddb65d092b42
Findlay20 <cameron.findlay@hotmail.co.uk>
2022-10-24 12:31:25 +0100

    Cleaned up displayPosts function

--- Client/script.js
+++ Client/script.js
@@ -26,50 +26,30 @@ toSendPost.addEventListener('submit',  e => {
 
 //Fetches all posts from the URL
 function getPosts () {
-    fetch('Stand-In-URL')
+    fetch('http://localhost:3000/')
         .then(allPosts => allPosts.json)
         .then(displayPosts) 
 }
 
+
 //Displays all posts - variables will need renamed to fit actual data
-async function displayPosts (data) {
+function displayPosts (data) {
     for (let i = 0; i < data.length; i++) {
         const post = data[i];
-
-        const newDiv = document.createElement('div');
-        newDiv.className = 'post';
-
-        const newEmojiButton = document.createElement('button');
-        newEmojiButton.className = 'emoji';
-        newEmojiButton.textContent = 'Emoji react';
-
-        const newH3 = document.createElement('h3');
-        newH3.textContent = `${post.title}`;
-
-
-        const newP = document.createElement('p');
-        newP.textContent = `${post.main}`;
-
-        const newCommentInput = document.createElement('input')
-        newCommentInput.type = 'text';
-        newCommentInput.placeholder = 'Add a comment...'
-
-        const newCommentSubmit = document.createElement('input');
-        newCommentSubmit.type = 'submit'
-        newCommentSubmit.value = 'Send';
-
-
-        postSection.append(newDiv);
-        newDiv.append(newEmojiButton);
-        newDiv.append(newH3);
-        newDiv.append(newP);
-        newDiv.append(newCommentInput);
-        newDiv.append(newCommentSubmit);
+        const markup = `
+        <div class="post">
+            <button class="emoji">Emoji react</button>
+            <h3>${post.title}</h3>
+            <p>${post.body}</p>
+            <input type="text" placeholder="Add a comment...">
+            <input type="submit" value="Send">
+        </div>`;
+        postSection.insertAdjacentHTML('beforeend', markup);
     }
-}
+};
 
 
 const exampleData = [{title: 'Hello World!',
-                    main: 'Blah blah blah blah blah blah blaaah ablblha bablb b blbfaaf ffhyllllb bbal e eqrppro  f y  y afhei  a;lf s.'}];
+                    body: 'Blah blah blah blah blah blah blaaah ablblha bablb b blbfaaf ffhyllllb bbal e eqrppro  f y  y afhei  a;lf s.'}];
 
 displayPosts(exampleData);
