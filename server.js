fe3be393d55e5d8a86c279f63f59929c51571ec9
Kelechi Okoli <kelechio1992@gmail.com>
2022-10-24 11:17:49 +0100

    Changed folder structure again to meet netlify deploment needs and added supporting packages

--- server.js
+++ server.js
@@ -1,16 +1,6 @@
-const path = require('path')
-const express = require('express')
-const cors = require('cors')
-
 const port = process.env.PORT || 3000
 
-const app = express()
-
-app.use(cors())
-app.use(express.json())
-app.use(express.static(path.join(__dirname, './client/assets')))
-
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, './client/index.html')))
+const app = require('./server/app')
 
 app.listen(port, (err) => {
     if (err) throw err
