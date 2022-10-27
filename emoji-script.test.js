2bb553c78ca685c9c554d3aa949cf60b78591558
Anna <anahx210@outlook.com>
2022-10-27 14:41:45 +0100

    emoji test script upload

---
+++ emoji-script.test.js
@@ -0,0 +1,36 @@
+import {activateEmojiButtons, displayEmojis} from './dist/assets/js/script'
+// import mockScript from './dist/assets/js/script';
+
+jest.mock('./dist/assets/js/script');
+
+
+// describe('activateEmojiButtons', () => {
+
+// })
+
+let reactionEmoji = (
+    { "type": "😀", "count": 9 },
+    { "type": "😥", "count": 2 },
+    { "type": "😮", "count": 1 }
+)
+// const section = document.querySelector('.emoji');
+
+const displayEmojis = jest.fn((emojis) => {
+    let markup = `<ul>`;
+    for (let i = 0; i < emojis.length; i++) {
+        markup += `<li>${emojis[i].type} ${emojis[i].count}</li>`
+    }
+    markup += `</ul>`
+    // section.insertAdjacentHTML('afterend', markup);
+    return markup;
+});
+
+describe('displayEmojis of a post', () => {
+    it('displays all reacted emojis per post', () => {
+        let result = displayEmojis(reactionEmoji);
+        // console.log(result)
+        expect(result) === (`<ul>
+        <li>😀 9</li><li>😥 2</li><li>😮 1</li>
+        </ul>`) 
+    })
+})
