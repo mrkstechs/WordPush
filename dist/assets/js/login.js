46f048f10450ef9536be7a7e4e6467620edb2262
Findlay20 <cameron.findlay@hotmail.co.uk>
2022-11-03 16:33:25 +0000

    Added registration function

--- dist/assets/js/login.js
+++ dist/assets/js/login.js
@@ -10,12 +10,29 @@ async function requestLogin(e){
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(Object.fromEntries(new FormData(e.target)))
         }
-        const r = await fetch(`http://localhost:3000/auth/login`, options)
+        const r = await fetch(`http://localhost:3000/login`, options)
         const data = await r.json()
         if (data.err){ throw Error(data.err); }
         login(data);
     } catch (err) {
         console.warn(`Error: ${err}`);
+    }
+}
+
+async function requestRegistration(e) {
+    e.preventDefault();
+    try {
+        const options = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(Object.fromEntries(new FormData(e.target)))
+        }
+        const r = await fetch(`http://localhost:3000/register`, options)
+        const data = await r.json()
+        if (data.err){ throw Error(data.err) }
+        requestLogin(e);
+    } catch (err) {
+        console.warn(err);
     }
 }
 
